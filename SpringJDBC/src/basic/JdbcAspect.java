4595b958b32a8cdae85830a13691127b3d844f10
Lahari276 <laharireddy147@gmail.com>
2021-04-26 22:07:57 +0530

    aspect

---
+++ SpringJDBC/src/basic/JdbcAspect.java
@@ -0,0 +1,12 @@
+package basic;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class JdbcAspect {
+	
+	@Before("execution(* jdbc..*.*(..))")
+	void log() {
+		System.out.println("before");
+	}
+}
